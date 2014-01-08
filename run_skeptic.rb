bb8edef98ca88e10852a1ea614e89681e551d7ad
hhursev <hhursev@gmail.com>
2014-01-08 07:17:33 +0200

    Rewrite run_skeptic script in slightly better manner

--- run_skeptic.rb
+++ run_skeptic.rb
@@ -1,23 +1,13 @@
 require 'yaml'
 
-if ARGV != []
-  RUN_IN_DIR  = ARGV[1]
-  CONFIG_FILE = ARGV[0]
+CONFIG_DICT = YAML.load_file(ARGV[0])
+RUN_IN_DIR  = ARGV[1]
 
-  skeptic_command = YAML.load_file(CONFIG_FILE).map { |k, v| v != true ? '--%s %s' % [k, v] : '--%s' % k }
-                                               .map { |k, _| k.gsub('_', '-') }
-                                               .join(' ')
-                                               .prepend('skeptic ')
+files   = Dir.entries(RUN_IN_DIR).map { |x| x.prepend RUN_IN_DIR if x.end_with? '.rb' }.compact
+command = CONFIG_DICT.map { |k, v| v == true ? '--%s' % k : '--%s %s' % [k, v] }
+                     .map { |opt| opt.gsub('_', '-') }
+                     .join(' ')
+                     .prepend('skeptic ')
 
-  ruby_files      = Dir.entries(RUN_IN_DIR).keep_if { |x| x.end_with? '.rb' }
-                                           .map     { |x| x.prepend RUN_IN_DIR }
-end
-
-exit_with = 'Kernel.exit(0)'
-
-ruby_files.each do |file|
-  output = system([skeptic_command, file].join(' '))
-  exit_with = 'Kernel.exit(1)' if output == false
-end
-
-eval exit_with
+Kernel.exit(1) if files.map { |file| system(command + ' ' + file) }.include? false
+Kernel.exit(0)

012c3220e78af5e759c555c8c92dc5ac2d4ae925
hhursev <hhursev@gmail.com>
2014-01-10 04:20:51 +0200

    Add coveralls in action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@
 $:<< File.join(File.dirname(__FILE__), '..')
 
 require "engine/board"
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true

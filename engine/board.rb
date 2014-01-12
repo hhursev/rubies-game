94942df3ac6fb30e01455519e6cbfc1ef8eb6348
hhursev <hhursev@gmail.com>
2014-01-12 17:03:49 +0200

    Add custom exceptions and implement them

--- engine/board.rb
+++ engine/board.rb
@@ -1,3 +1,22 @@
+class MustTakeFromOneRow < StandardError
+  def initialize(message="Rubies you take must be from one row")
+    super(message)
+  end
+end
+
+class RubiesNotConnected < StandardError
+  def initialize(message="Rubies are not connected (read the rules)")
+    super(message)
+  end
+end
+
+class OutOfBoard < StandardError
+  def initialize(message="You make actions out of the board's reach")
+    super(message)
+  end
+end
+
+
 class RubiesBoard
   attr_reader :board
 
@@ -8,9 +27,21 @@ class RubiesBoard
   end
 
   def picture
+    #must find out the longest column
+    #based on it to arrange the rest
+    #and centralize them beautifully
+
+    #find out how to test this as
+    #we have random board always...
   end
 
   def take_out(*positions)
+    columns = positions.flat_map { |_, column| column }
+    raise OutOfBoard         if positions.map { |row, column| filled?(row, column) }.include? nil
+    raise MustTakeFromOneRow if positions.map { |row, _| row }.uniq.size > 1
+    if columns.size > 1
+      raise RubiesNotConnected if columns.map { |x| (columns & [x + 1, x - 1]).any? }.include? false
+    end
     positions.map { |row, column| empty(row, column) }
   end
 

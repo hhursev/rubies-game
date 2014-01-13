652e49fe325092938c3c1f9b9c5d5c7ae1ae0b26
hhursev <hhursev@gmail.com>
2014-01-14 00:17:16 +0200

    First more serious skeleton prototype of the AI

--- engine/AI.rb
+++ engine/AI.rb
@@ -19,6 +19,23 @@ class AI
 
     def decision
     end
+
+    private
+
+    # function that takes exactly specific count of rubies
+    # function that takes random number of rubies
+    # function that calculates the num sum
+    # function that check if only single matches + bigget stack are available
+    def nim_sum(*stacks)
+      stacks.inject(&:^)
+    end
+
+    def break_stacks_in_power_of_twos
+    end
+
+    def power_of_2?(number)
+      number.to_s(2).scan(/1/).size == 1
+    end
   end
 
   class JuniorAlgorithm

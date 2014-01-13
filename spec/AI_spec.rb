85c9255e1f492e6120c3bd9ba19dc0f40c8cf093
hhursev <hhursev@gmail.com>
2014-01-14 00:14:50 +0200

    Predefine RubiesBoard initialize function
    This is only done in the spec file, so to be able
    to create custom board fields on which to be free
    to create test cases and edge cases for the AI engine

--- spec/AI_spec.rb
+++ spec/AI_spec.rb
@@ -1,5 +1,23 @@
 require "spec_helper"
 
+class RubiesBoard
+  def initialize(rows: 5, custom_board: false)
+    @rows = rows
+    custom_board ? @board = custom_board : @board = {}
+    initialize_board custom_board
+  end
+
+  private
+
+  def initialize_board(custom_board)
+    if custom_board
+      board.keys.map { |row, column| fill row, column }
+    else
+      1.upto(@rows).map { |row| fill_column row, rand(row..row*2) }
+    end
+  end
+end
+
 describe "AI" do
   describe "MasterAlgorithm" do
     let (:ai)               { make_ai }
@@ -10,6 +28,12 @@ describe "AI" do
     it "check that algorithms respond to decision" do
       master_algorithm.should respond_to :decision
       junior_algorithm.should respond_to :decision
+    end
+
+    it "tests if i can create custom board" do
+      board_hash = { [1, 1] => nil, [2, 1] => nil, [2, 2] => nil }
+      custom_board = make_board(custom_board: board_hash)
+      custom_board.board.keys().should eq board_hash.keys()
     end
 
     def master_algorithm(*args)
@@ -29,3 +53,5 @@ end
 def make_ai(*args)
   args.empty? ? AI.new(make_board) : AI.new(*args)
 end
+
+

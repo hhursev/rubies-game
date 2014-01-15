9b2a9f6cb8d5bc451c79c0699596d09f420f5dc9
hhursev <hhursev@gmail.com>
2014-01-15 13:46:33 +0200

    Add some tests validating the AI logic and correctness

--- spec/AI_spec.rb
+++ spec/AI_spec.rb
@@ -58,14 +58,21 @@ describe "AI" do
       junior_algorithm.should respond_to :stacks
     end
 
-    it "checks that algorithms respond to nim_sum_zero?" do
+    it "checks that master algorithm responds to nim_sum_zero?" do
       master_algorithm.should respond_to :nim_sum_zero?
-      junior_algorithm.should respond_to :nim_sum_zero?
     end
 
     it "checks that algorithms respond to decision" do
       master_algorithm.should respond_to :decision
       junior_algorithm.should respond_to :decision
+    end
+
+    it "checks that master_algorithm responds to perfect move method" do
+      master_algorithm.should respond_to :perfect_move
+    end
+
+    it "checks that master_algorithm responds to how_many_rubies_from_which_stack" do
+      master_algorithm.should respond_to :how_many_rubies_from_which_stack
     end
 
     it "verifies that the AI initialization works the right way" do
@@ -150,9 +157,146 @@ describe "AI" do
       test_ai.nim_sum_zero?.should eq true
     end
 
-    def master_algorithm(*args)
-      args.empty? ? AI::MasterAlgorithm.new(board) : AI::MasterAlgorithm.new(*args)
+    it "verifies MasterAlgorithm plays the perfect move and makes nim sum zero" do
+      <<-DENTED_BOARD_1_2_3_5_8_vol3
+            x
+           o x
+          x o o
+         x x o x x
+      x o x o x x x x
+      DENTED_BOARD_1_2_3_5_8_vol3
+
+      board_hash   = BOARD_1_2_3_5_8
+      custom_board = make_board custom_board: board_hash
+      custom_board.take_out [2, 1]
+      custom_board.take_out [3, 2], [3, 3]
+      custom_board.take_out [4, 3]
+      custom_board.take_out [5, 2]
+      custom_board.take_out [5, 4]
+      test_ai = make_ai custom_board, difficulty=:hard
+      test_ai.moves.should eq [[5, 8], [5, 7], [5, 6]]
     end
+
+    it "make sure algorithm catches cases where odd number of single rubies and one big stack on board" do
+      <<-DENTED_BOARD_1_2_3_5_8_vol4
+            x
+           o x
+          o o o
+         o o o o x
+      o o o o x x x x
+      DENTED_BOARD_1_2_3_5_8_vol4
+
+      board_hash   = BOARD_1_2_3_5_8
+      custom_board = make_board custom_board: board_hash
+      custom_board.take_out [2, 1]
+      custom_board.take_out [3, 1], [3, 2], [3, 3]
+      custom_board.take_out [4, 1], [4, 2], [4, 3], [4, 4]
+      custom_board.take_out [5, 1], [5, 2], [5, 3], [5, 4]
+      test_ai = make_ai custom_board, difficulty=:hard
+      test_ai.moves.should eq [[5, 8], [5, 7], [5, 6], [5, 5]]
+    end
+
+    it "make sure algorithm catches cases where even number of single rubies and one big stack on board" do
+      <<-DENTED_BOARD_1_2_3_5_8_vol5
+            x
+           o x
+          o o o
+         o o o o x
+      x o o o x x x x
+      DENTED_BOARD_1_2_3_5_8_vol5
+
+      board_hash   = BOARD_1_2_3_5_8
+      custom_board = make_board custom_board: board_hash
+      custom_board.take_out [2, 1]
+      custom_board.take_out [3, 1], [3, 2], [3, 3]
+      custom_board.take_out [4, 1], [4, 2], [4, 3], [4, 4]
+      custom_board.take_out [5, 2], [5, 3], [5, 4]
+      test_ai = make_ai custom_board, difficulty=:hard
+      test_ai.moves.should eq [[5, 8], [5, 7], [5, 6]]
+    end
+
+    it "takes ruby when even number of single rubies left" do
+      <<-DENTED_BOARD_1_2_3_5_8_vol6
+            x
+           o x
+          o o o
+         o o o o x
+      x o o o o o o o
+      DENTED_BOARD_1_2_3_5_8_vol6
+
+      board_hash   = BOARD_1_2_3_5_8
+      custom_board = make_board custom_board: board_hash
+      custom_board.take_out [2, 1]
+      custom_board.take_out [3, 1], [3, 2], [3, 3]
+      custom_board.take_out [4, 1], [4, 2], [4, 3], [4, 4]
+      custom_board.take_out [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8]
+      test_ai = make_ai custom_board, difficulty=:hard
+      position = test_ai.moves
+
+      position.size.should eq 1
+      position.is_a?(Array).should eq true
+      custom_board.filled_at?(position[0].first, position[0].last).should eq true
+    end
+
+    it "takes ruby when odd number of single rubies left" do
+      <<-DENTED_BOARD_1_2_3_5_8_vol7
+            x
+           o x
+          o o o
+         o o o o o
+      x o o o o o o o
+      DENTED_BOARD_1_2_3_5_8_vol7
+
+      board_hash   = BOARD_1_2_3_5_8
+      custom_board = make_board custom_board: board_hash
+      custom_board.take_out [2, 1]
+      custom_board.take_out [3, 1], [3, 2], [3, 3]
+      custom_board.take_out [4, 1], [4, 2], [4, 3], [4, 4], [4, 5]
+      custom_board.take_out [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8]
+      test_ai = make_ai custom_board, difficulty=:hard
+      position = test_ai.moves
+
+      position.size.should eq 1
+      position.is_a?(Array).should eq true
+      custom_board.filled_at?(position[0].first, position[0].last).should eq true
+    end
+
+    it "verifies that AI's random take is by the rules" do
+      <<-DENTED_BOARD_1_2_3_5_8_vol8
+            x
+           o x
+          x o o
+         x x o x x
+      x o x o x x x x
+      DENTED_BOARD_1_2_3_5_8_vol8
+
+      board_hash   = BOARD_1_2_3_5_8
+      row, column = 0, 1
+      custom_board = make_board custom_board: board_hash
+      custom_board.take_out [2, 1]
+      custom_board.take_out [3, 2], [3, 3]
+      custom_board.take_out [4, 3]
+      custom_board.take_out [5, 2]
+      custom_board.take_out [5, 4]
+      test_ai   = make_ai custom_board, difficulty=:junior
+
+      positions = test_ai.moves
+
+      rows    = positions.map { |position| position[row] }
+      columns = positions.map { |position| position[column] }
+
+      rows.uniq.size.should eq 1
+      positions.each do |position|
+        if columns.size > 1
+          (columns & [position[column] + 1, position[column] - 1]).any?.should eq true
+        end
+        custom_board.filled_at?(position[row], position[column]).should eq true
+      end
+    end
+
+    def master_algorithm(*args)
+        args.empty? ? AI::MasterAlgorithm.new(board) : AI::MasterAlgorithm.new(*args)
+      end
 
     def junior_algorithm(*args)
       args.empty? ? AI::JuniorAlgorithm.new(board) : AI::JuniorAlgorithm.new(*args)

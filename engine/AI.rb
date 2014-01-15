5605cec82f760bcdc03ea2d0470f80defb8e8e45
hhursev <hhursev@gmail.com>
2014-01-15 13:47:01 +0200

    Add MasterAlgorithm and JuniorAlgorithm logic

--- engine/AI.rb
+++ engine/AI.rb
@@ -1,5 +1,3 @@
-require_relative 'board'
-
 class AI
   attr_reader :rubies_board, :difficulty
 
@@ -28,18 +26,68 @@ class AI
       end.flatten.reject { |stack| stack.zero? }
     end
 
-    def nim_sum_zero?
-      stacks.inject(&:^).zero? ? true : false
+
+    def random_take
+      rubies_to_take = rand(1..stacks.max)
+      take rubies_to_take, stacks.keep_if { |stack| stack >= rubies_to_take }.sample()
+    end
+
+    def single_rubies_with_stack
+      take((stacks.size.even? ? stacks.max : stacks.max - 1), stacks.max)
+    end
+
+    def take(rubies_count, from_stack_sized)
+      row, column = locate_stack(from_stack_sized)
+      (0...rubies_count).map { |x| [row, column - x] }
+    end
+
+    def locate_stack(stack_size)
+      @rubies_board.board.keys.sort.group_by(&:first).values().map do |row|
+        connected_rubies = 0
+        row.map do |row, column|
+          @rubies_board.filled_at?(row, column) ? connected_rubies += 1 : connected_rubies = 0
+          if connected_rubies == stack_size and not @rubies_board.filled_at?(row, column + 1)
+            return [row, column]
+          end
+        end
+      end
     end
   end
 
   class MasterAlgorithm < BaseAlgorithmFunctionality
     def decision
+      return single_rubies_with_stack if stacks.count(1).eql?(stacks.size - 1)
+      return random_take              if nim_sum_zero?
+      return perfect_move
+    end
+
+    def nim_sum_zero?
+      stacks.reduce(&:^).zero? ? true : false
+    end
+
+    def perfect_move
+      take(*how_many_rubies_from_which_stack)
+    end
+
+    def how_many_rubies_from_which_stack
+      altered_stacks = stacks.sort.reverse
+      altered_stacks.each_with_index do |stack, index|
+        (1..stack).each do |rubies|
+          altered_stacks[index] -= rubies
+          return [rubies, altered_stacks[index] + rubies] if altered_stacks.reduce(&:^).zero?
+          altered_stacks[index] += rubies
+        end
+      end
     end
   end
 
   class JuniorAlgorithm < BaseAlgorithmFunctionality
     def decision
+      if stacks.count(1).eql?(stacks.size - 1)
+        single_rubies_with_stack
+      else
+        random_take
+      end
     end
   end
 end

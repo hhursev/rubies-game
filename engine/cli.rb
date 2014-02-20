c0285323de257b7afdb8da7b503f4980cd625b8d
hhursev <hhursev@gmail.com>
2014-02-20 06:03:06 +0200

    Transform cli to less optional and tedious one

--- engine/cli.rb
+++ engine/cli.rb
@@ -1,43 +1,23 @@
 # $:.unshift File.dirname(__FILE__)
-require_relative "board"
-require_relative "game"
-require_relative "ai"
+require "./board"
+require "./game"
+require "./ai"
 
 class CLIGameplay
   def initialize
-    @computer, @play_vs_ai, @ai_difficulty, @start_second_vs_ai, @load = nil
-    play_new_game_or_load?
-    if @load == "load"
-      load_game
-    else
-      initialize_game
-    end
+    @computer, @ai_difficulty, @start_second_vs_ai, @load = nil
+    initialize_game
     play_game
   end
 
   private
 
-  def play_new_game_or_load?
-    until @load == "load" or @load == "new"
-      puts "do you want to load from saved game or start new one load/new"
-      @load = gets.chomp
-    end
-  end
-
   def play_vs_ai?
-    until @play_vs_ai == "y" or @play_vs_ai == "n"
-      puts "do you want to play vs AI? y/n"
-      @play_vs_ai = gets.chomp
-    end
+    @play_vs_ai = "y"
   end
 
   def ai_difficulty?
-    if @play_vs_ai == "y"
-      until @ai_difficulty == "hard" or @ai_difficulty == "easy"
-        puts "select ai difficulty: hard/easy"
-        @ai_difficulty = gets.chomp
-      end
-    end
+    @ai_difficulty = "easy"
   end
 
   def initialize_game
@@ -51,23 +31,11 @@ class CLIGameplay
   end
 
   def initialize_game_vs_ai
-    start_second_vs_ai?
-    if @start_second_vs_ai == "y"
-      @game = RubiesGame.new(RubiesBoard.new(), "computer", "human_player", @ai_difficulty)
-    else
-      @game = RubiesGame.new(RubiesBoard.new(), "human_player", "computer", @ai_difficulty)
-    end
+    @game = RubiesGame.new(RubiesBoard.new(), "computer", "human_player", @ai_difficulty)
   end
 
   def initialize_game_vs_human
     @game = RubiesGame.new(RubiesBoard.new(), "first_player", "second_player")
-  end
-
-  def start_second_vs_ai?
-    until @start_second_vs_ai == "y" or @start_second_vs_ai == "n"
-      puts "do you want to start second y/n"
-      @start_second_vs_ai = gets.chomp
-    end
   end
 
   def play_game
@@ -101,23 +69,6 @@ class CLIGameplay
       @game.human_move(player_move)
     rescue Exception => e
       puts e.message
-    end
-  end
-
-  def save_game(player_move)
-    player_move.slice!("save")
-    RubiesGame.dump_to_file(@game, player_move.strip.tr("/\?:*<>: ","_"))
-  end
-
-  def load_game
-    saved_games = Dir.entries("saved_games").select { |entry| entry != '.' and entry != '..' }
-                                            .join("\n")
-
-    puts "---- LIST OF SAVED GAMES ----\n #{saved_games} \n-------               -------\n"
-
-    until @game != nil
-      load_game = gets.chomp
-      @game = Marshal.load(File.read("saved_games/#{load_game}"))
     end
   end
 end

f839d6ed0631f2a8b13514ee89900fc39366e45b
hhursev <hhursev@gmail.com>
2014-03-06 13:18:13 +0200

    Add tests for whose turn it is

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -32,6 +32,19 @@ describe "RubiesGame" do
     game.winner.should eq nil
   end
 
+  it "checks if on_move_is returns correct stuff from beginnig" do
+    game.on_move_is.should eq 'first_player'
+  end
+
+  it "checks that on_move_is changes when correct move is made" do
+    game.make_move([[1, 1]])
+    game.on_move_is.should eq 'second_player'
+  end
+
+  it "verifies that make_move method raises error when invalid move is submited" do
+    expect { game.make_move([[1, 1], [1, 2], [1, 3]]) }.to raise_error
+  end
+
   def make_board(*args)
     RubiesBoard.new(*args)
   end
